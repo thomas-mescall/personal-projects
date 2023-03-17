ddd51225749cf80a4e41f208ceee127fd8b29a33
thomas-mescall <122935621+thomas-mescall@users.noreply.github.com>
2023-03-16 21:35:35 -0500

    finished tweenjs intro

---
+++ js/tweenjs-intro.js
@@ -0,0 +1,18 @@
+"use strict";
+
+function init() {
+    let stage = new createjs.Stage("demoCanvas");
+    let circle = new createjs.Shape();
+    circle.graphics.beginFill("Red").drawCircle(0,0,50);
+    circle.x = 100;
+    circle.y = 100;
+    stage.addChild(circle);
+    createjs.Tween.get(circle, {loop: true})
+        .to({x: 400}, 1000, createjs.Ease.getPowInOut(4))
+        .to({alpha: 0, y: 175}, 500, createjs.Ease.getPowInOut(2))
+        .to({alpha: 0, y: 225}, 100)
+        .to({alpha: 1, y: 200}, 500,createjs.Ease.getPowInOut(2))
+        .to({x: 100}, 800, createjs.Ease.getPowInOut(2));
+    createjs.Ticker.setFPS(60);
+    createjs.Ticker.addEventListener("tick", stage);
+}
